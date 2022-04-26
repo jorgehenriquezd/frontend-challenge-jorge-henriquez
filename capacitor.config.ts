ddbafa748ee46ff356f388bc7d0ddc7e19a88fee
jorgehenriquezd <57832978+jorgehenriquezd@users.noreply.github.com>
2022-04-26 17:46:15 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'frontend-challenge-jorge-henriquez',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
